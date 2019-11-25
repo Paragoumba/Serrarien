35deedebde0132913225a80c26ce43796cf81921
Paragoumba <paragoumba@gmail.com>
2019-11-25 07:10:22 +0100

    Switched to OpenGL 3.3

--- engine/Window.cpp
+++ engine/Window.cpp
@@ -8,7 +8,7 @@
 Window::Window(const char* _title, int _width, int _height) : title(_title), cursor(nullptr){
 
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
